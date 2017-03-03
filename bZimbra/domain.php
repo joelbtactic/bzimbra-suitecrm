36b7a267598e9aae62ecaa8637f0e4180bc9d095
Marc Sánchez <sanchezfauste@gmail.com>
2017-03-03 13:54:21 +0100

    Moving set_attribute() and set_mb_attribute() to utils

--- bZimbra/domain.php
+++ bZimbra/domain.php
@@ -26,25 +26,25 @@ abstract class Domain {
         $bean = retrieve_record_bean('btc_bMail', $keys_values);
         $bean->name = $domain->name;
         $domain_atributes = get_atributes_as_array($domain->a);
-        self::set_mb_attribute($domain_atributes, $bean,
+        set_mb_attribute($domain_atributes, $bean,
                 'zimbraMailDomainQuota', 'capacidad');
-        self::set_mb_attribute($domain_atributes, $bean,
+        set_mb_attribute($domain_atributes, $bean,
                 'zimbraDomainAggregateQuota', 'capacidad_agregado');
-        self::set_attribute($domain_atributes, $bean,
+        set_attribute($domain_atributes, $bean,
                 'zimbraDomainAggregateQuotaWarnPercent', 'warn_percent');
-        self::set_attribute($domain_atributes, $bean,
+        set_attribute($domain_atributes, $bean,
                 'zimbraDomainAggregateQuotaWarnEmailRecipient', 'email_quota_warn');
-        self::set_attribute($domain_atributes, $bean,
+        set_attribute($domain_atributes, $bean,
                 'zimbraDomainAggregateQuotaPolicy', 'aggregate_quota_policy');
-        self::set_attribute($domain_atributes, $bean,
+        set_attribute($domain_atributes, $bean,
                 'zimbraPulicServiceHostname', 'nombre_servidor_publico');
-        self::set_attribute($domain_atributes, $bean,
+        set_attribute($domain_atributes, $bean,
                 'zimbraPublicServicePort', 'puerto_servicio_publico');
-        self::set_attribute($domain_atributes, $bean,
+        set_attribute($domain_atributes, $bean,
                 'zimbraPublicServiceProtocol', 'protocolo_servicio_publico');
-        self::set_attribute($domain_atributes, $bean, 'description', 'description');
+        set_attribute($domain_atributes, $bean, 'description', 'description');
         //self::set_attribute($domain_atributes, $bean, 'zimbraDomainStatus', '');
-        self::set_attribute($domain_atributes, $bean,
+        set_attribute($domain_atributes, $bean,
                 'zimbraDomainDefaultCOSId', 'id_clase_servicio_por_defecto');
         $bean->save();
         self::relate_zimbra_domain_with_domain($bean, $domain->name);
@@ -57,16 +57,6 @@ abstract class Domain {
         if (!empty($domain_bean->id)) {
             $domain_bean->load_relationship('btc_dominios_btc_bmail');
             $domain_bean->btc_dominios_btc_bmail->add($zimbra_bean);
-        }
-    }
-
-    static private function set_attribute($domain_info, $bean, $dinfo_var, $bean_var) {
-        if(isset($domain_info[$dinfo_var])) $bean->$bean_var = $domain_info[$dinfo_var];
-    }
-
-    static private function set_mb_attribute($domain_info, $bean, $dinfo_var, $bean_var) {
-        if(isset($domain_info[$dinfo_var])) {
-            $bean->$bean_var = bytes_to_megabytes($domain_info[$dinfo_var]);
         }
     }
 
