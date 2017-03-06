9bca369d3e42cc36c915555069f4a5f47559c6db
Marc Sánchez <sanchezfauste@gmail.com>
2017-03-06 12:43:10 +0100

    Syncing accounts quotas

--- bZimbra/main.php
+++ bZimbra/main.php
@@ -2,11 +2,13 @@
 
 require_once('bZimbra/domain.php');
 require_once('bZimbra/account.php');
+require_once('bZimbra/quota.php');
 
 function main() {
     $GLOBALS['log']->fatal("[bZimbra] Entering bZimbra synchronization.");
     Domain::sync_all_domains();
     Account::sync_all_accounts();
+    Quota::sync_all_quotas();
     $GLOBALS['log']->fatal("[bZimbra] bZimbra synchronization finished.");
     return true;
 }
---
+++ bZimbra/quota.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once('bZimbra/zimbra_api.php');
+require_once('bZimbra/utils.php');
+require_once('bZimbra/bean_utils.php');
+
+abstract class Quota {
+
+    static public function sync_all_quotas() {
+        foreach (ZimbraAPI::get_zimbra_servers() as $servername => $server_properties) {
+            $GLOBALS['log']->fatal("[bZimbra] Syncing accounts quotas of '".$servername
+                    ."' server...");
+            self::sync_all_quotas_of_server($server_properties);
+        }
+    }
+
+    static public function sync_all_quotas_of_server($server_properties) {
+        $api_instance = ZimbraAPI::get_api_instance($server_properties);
+        $quotas = $api_instance->getQuotaUsage()->account;
+        foreach ($quotas as $quota) {
+            self::sync_quota((array) $quota);
+        }
+        $GLOBALS['log']->fatal("[bZimbra] --> ".count($quotas)." Zimbra quotas synced.");
+    }
+
+    static public function sync_quota($quota) {
+        $keys_values = array();
+        $keys_values['name'] = $quota['name'];
+        $bean = retrieve_record_bean('btc_Zimbra_Accounts', $keys_values);
+        $bean->name = $quota['name'];
+        set_mb_attribute($quota, $bean, 'limit', 'quota');
+        set_mb_attribute($quota, $bean, 'used', 'usado');
+        $bean->save();
+    }
+
+}
+
+?>
