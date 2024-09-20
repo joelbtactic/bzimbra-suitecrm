765312a87c7e6ba26c941682ba943da12b914429
joelbtactic <joel.ampurdanes@btactic.com>
2024-09-20 17:01:29 +0200

    refactor: update imports and class name from Quota to AccountQuota

--- bZimbra/main.php
+++ bZimbra/main.php
@@ -2,14 +2,14 @@
 
 require_once('bZimbra/domain.php');
 require_once('bZimbra/account.php');
-require_once('bZimbra/quota.php');
+require_once('bZimbra/account_quota.php');
 require_once('bZimbra/domain_quota.php');
 
 function main() {
     $GLOBALS['log']->fatal("[bZimbra] Entering bZimbra synchronization.");
     Domain::sync_all_domains();
 //    ZimbraAccount::sync_all_accounts();
-//    Quota::sync_all_quotas();
+//    AccountQuota::sync_all_quotas();
     DomainQuota::sync_all_domains();
     $GLOBALS['log']->fatal("[bZimbra] bZimbra synchronization finished.");
     return true;
