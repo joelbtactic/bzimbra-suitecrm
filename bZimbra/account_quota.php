cacc8c22eec5c7f0f29dc8cfc9fd4f1ade1e5556
joelbtactic <joel.ampurdanes@btactic.com>
2024-09-20 16:52:54 +0200

    refactor: rename and modify quota.php to account_quota.php

---
+++ bZimbra/account_quota.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once('bZimbra/zimbra_api.php');
+require_once('bZimbra/utils.php');
+require_once('bZimbra/bean_utils.php');
+
+abstract class AccountQuota {
+
+    // These two method are not used, instead, the sync_all_quotas_of_demain method is used
+    // static public function sync_all_quotas() {
+    //     foreach (ZimbraAPI::get_zimbra_servers() as $servername => $server_properties) {
+    //         $GLOBALS['log']->fatal("[bZimbra] Syncing accounts quotas of '".$servername
+    //                 ."' server...");
+    //         self::sync_all_quotas_of_server($server_properties);
+    //     }
+    // }
+
+    // static public function sync_all_quotas_of_server($server_properties) {
+    //     $api_instance = ZimbraAPI::get_api_instance($server_properties);
+    //     $quotas = $api_instance->getQuotaUsage()->account;
+    //     foreach ($quotas as $quota) {
+    //         self::sync_quota((array) $quota);
+    //     }
+    //     $GLOBALS['log']->fatal("[bZimbra] --> ".count($quotas)." Zimbra quotas synced.");
+    // }
+
+    static public function sync_all_quotas_of_domain($api_instance, $domain) {
+        $quotas = $api_instance->getQuotaUsage($domain, true);
+        $accounts_quotas = $quotas->getAccountsQuotas();
+        if (!isset($accounts_quotas)) {
+             $GLOBALS['log']->fatal("[bZimbra] --> Domain '".$domain."' has no "
+                     ."accounts quotas to sync. Maybe it's an alias.");
+             return;
+        }
+        foreach ($quotas->getAccountsQuotas() as $quota) {
+            self::sync_quota($quota);
+        }
+        $GLOBALS['log']->fatal("[bZimbra] --> ".count($quotas->getAccountsQuotas())
+                ." Zimbra quotas synced from domain '".$domain."'.");
+    }
+
+    static public function sync_quota($quota) {
+        $keys_values = array();
+        $keys_values['name'] = $quota['name'];
+        $bean = retrieve_record_bean('btc_Zimbra_Accounts', $keys_values);
+        $bean->name = $quota['name'];
+        set_quota_limit_attr($quota, $bean,'quota');
+        set_quota_used_attr($quota, $bean,'usado');
+        $bean->save();
+    }
+
+}
+
+?>
